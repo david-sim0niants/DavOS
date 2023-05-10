a6f4124f9e1259c6573b0553ef76f4134903e031
David <david.simonyants@gmail.com>
2023-05-10 11:04:02 +0400

    Fixed i386 image label names by introducing a better way to convert dumped sections into object files.

--- arch/x86/scripts/dump_sections_i386.sh
+++ arch/x86/scripts/dump_sections_i386.sh
@@ -2,24 +2,34 @@
 
 filename=$1
 target_format=$2
+cc=$3
 
-sections=( 	".multiboot_header 	alloc,load,readonly"
-		".text 			alloc,load,code,readonly"
-		".data 			alloc,load"
-		".rodata 		alloc,load,readonly")
+section_memlayout="$(dirname $0)/section_memlayout.S"
+
+sections=( 	"multiboot_header 	a"
+		"text 			ax"
+		"data 			wa"
+		"rodata 		a")
 
 for section in "${sections[@]}"
 do
-	read -r section_name section_flags <<< "$section"
+	read -r section_label_name section_flags <<< "$section"
+
+	section_name=.$section_label_name
+	new_section_name=$section_name.i386
+	new_section_label_name=${section_label_name}_i386_start
+
 	# define raw binary and non-binary object filenames
 	section_bin_filename=$filename$section_name.bin
 	section_obj_filename=$filename$section_name.o
 
 	# dump current section as a raw binary object file
 	objcopy --dump-section $section_name=$section_bin_filename $filename &&\
-	# convert the raw binary section object into the target format
-	# and reset the section name and flags
-	objcopy -I binary -O $target_format --rename-section .data=$section_name.i386 --set-section-flags .data=$section_flags $section_bin_filename $section_obj_filename && \
+	# convert the raw binary section object into the target format and
+	# set section name, section flags
+	# gcc will compile from an assembly file template, which will include
+	# $section_bin_filename raw binary bytes in it
+	$cc -c $section_memlayout -o $section_obj_filename -DSECTION_NAME=$new_section_name -DSECTION_LABEL=$new_section_label_name -DSECTION_FLAGS=\"$section_flags\" -D__BINARY_FILE__=\"$section_bin_filename\" &&\
 	# remove the temporary raw binary object
 	rm $section_bin_filename &&\
 	# output object filename if objcopying succeeded, otherwise don't
